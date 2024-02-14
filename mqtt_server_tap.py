fb4a5415da6c2284fef6ad209f1de23e3f31b7f2
rtitod <rtitod@unsa.edu.pe>
2024-02-14 04:43:16 -0500

    Se corrigieron bugs importantes

--- mqtt_server_tap.py
+++ mqtt_server_tap.py
@@ -21,21 +21,30 @@ def on_message(client, userdata, msg):
     payload = msg.payload.decode("utf-8")
     print("Mensaje recibido en el topico {}: {}".format(msg.topic, payload))
     client_publish = mqtt.Client()
-    client_publish.connect(broker_address, port, 60)
+    client_publish.connect(broker_address, port, 10)
     client_publish.loop_start()
     client_publish.publish(topicsartifact, payload, qos=1)
     print("Mensaje enviado a Edge")
     time.sleep(1)
     client_publish.disconnect()
 
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("Conectado. Esperando mensajes en el topico {}".format(topicsartifact))
+    client.subscribe(amazon_topic_artifact)
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Conexion perdida. Codigo de resultado: {}. Intentando reconectar...".format(rc))
+
 client_suscribe = mqtt.Client()
 client_suscribe.tls_set(ca_file, certfile=cert_file, keyfile=key_file, cert_reqs=ssl.CERT_NONE, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 client_suscribe.on_message = on_message
-client_suscribe.connect(amazon_broker_address, 8883, 60)
-client_suscribe.subscribe(amazon_topic_artifact)
+client_suscribe.on_disconnect = on_disconnect
+client_suscribe.on_connect = on_connect
 
 try:
-    print("Conectado al servidor MQTT. Esperando mensajes en el topico {}...".format(topicsartifact))
+    client_suscribe.connect_async(amazon_broker_address, 8883, 60)
+    print("Conectando al servidor MQTT...")
     client_suscribe.loop_forever()
 
 except KeyboardInterrupt:
